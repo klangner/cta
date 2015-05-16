9e0f5e888ff7ea7e33925c75bc186c30dbfa1cbe
klangner <klangner@gmail.com>
2015-05-16 10:42:17 +0200

    init

---
+++ src/main/java/com/github/cta/Main.java
@@ -0,0 +1,11 @@
+package com.github.cta;
+
+/**
+ * @author Krzysztof Langner on 16.05.15.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
